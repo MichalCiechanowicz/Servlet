e453798ec6d7cfee950ac9f0ed2c9da6f914ea5f
MichalCiechanowicz <emgasuk@gmail.com>
2019-09-15 12:09:54 +0100

    ServletToServlet_First

--- src/main/java/com/sda/pl/first/ServletToServlet.java
+++ src/main/java/com/sda/pl/first/ServletToServlet.java
@@ -7,18 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class ServletToServlet {
 
-    @WebServlet(name="RandomJson", urlPatterns = {"/getRandomNumber", "/RandomJson"})
-    public class RandomJson extends HttpServlet {
+@WebServlet(name = "ServletToServlet", urlPatterns = {"/servletToServlet"})
+public class ServletToServlet extends HttpServlet {
 
-        @Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse
-                response) throws IOException {
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
-            out.println("jestes tu gdzie chciales byc");
-        }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse
+            response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("jestes tu gdzie chciales byc");
     }
-
 }
+
