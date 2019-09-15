20c487a756f02d62a722666217a4c94bbc30aa8c
MichalCiechanowicz <emgasuk@gmail.com>
2019-09-15 12:36:41 +0100

    ServletToServlet_condition

--- src/main/java/com/sda/pl/first/ServletToServlet.java
+++ src/main/java/com/sda/pl/first/ServletToServlet.java
@@ -11,12 +11,18 @@ import java.io.PrintWriter;
 @WebServlet(name = "ServletToServlet", urlPatterns = {"/servletToServlet"})
 public class ServletToServlet extends HttpServlet {
 
+    private static final String EXPECTED_VALUE = "true";
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse
-            response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("jestes tu gdzie chciales byc");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String parametr = request.getParameter("random");
+        if(EXPECTED_VALUE.equals(parametr)){
+
+        }else {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("jestes tu gdzie chciales byc");
+        }
     }
 }
 
